790a5874a20eb78e78b83c13ab53feeed620d7ff
CodingBee77 <00martyna.znajdek00@gmail.com>
2021-11-17 18:58:59 +0100

    Refactor Main Container

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -1,10 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
-
-
-import SearchButton from '../components/SearchButton'
-import PlayButton from '../components/PlayButton'
-
+import Button from '../components/Button';
 
 export default class MainContainer extends Component {
 
@@ -12,8 +8,8 @@ export default class MainContainer extends Component {
   render() {
     return(
       <div>
-        <Link to="/search"><SearchButton /></Link>
-        <Link to="/game"><PlayButton /></Link>
+        <Link to="/search"><Button message='Search!' /></Link>
+        <Link to="/game"><Button message='Play!' /></Link>
 
       </div>
     )
